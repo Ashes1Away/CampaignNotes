3911175a60d135c2b45db4c6342ef9a991b151b1
Ashes1Away <ashes1away@gmail.com>
2024-03-16 02:23:38 +0000

    Quartz sync: Mar 16, 2024, 2:23 AM

--- content/index.md
+++ content/index.md
@@ -2,5 +2,6 @@
 title: Welcome to Quartz
 ---
 [[Public/index|index]] [[DM/index|DM]]
+[[Public/displayable|Testable]]
 test
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Ashes' Campaign Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ashes1away.github.io/CampaignNotes",
     ignorePatterns: ["private", "templates", ".obsidian","DM"],
     defaultDateType: "created",
     theme: {
